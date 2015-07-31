8d0063fc8bc18c303220f6ac6c804043413304db
Nathan Myers <ncm@asperasoft.com>
2015-07-30 21:15:08 -0400

    internal docs, cleaner code

--- puzzlegen.cc
+++ puzzlegen.cc
@@ -7,10 +7,6 @@
 #include <iostream>
 
 using Letters = int;
-struct Word {
-    Letters letters;
-    size_t length;
-};
 
 int main(int ac, char** av)
 {
@@ -22,18 +18,18 @@ int main(int ac, char** av)
        return 1;
     }
 
-    std::vector<Word> words;
+    std::vector<Letters> words;
     std::set<Letters> sevens;
     std::istream_iterator<std::string> it(in), end;
     std::for_each(it, end,
-        [&words,&sevens](auto&& word) {
+        [&](auto&& word) {
             Letters letters = std::accumulate(word.begin(), word.end(), 0,
                 [](Letters a, char b) {
                     return a | ((b >= 'a' && b <= 'z') ? (1 << (b - 'a')) : -1);
                 }
             );
             if (letters > 0 && word.size() >= 5) {
-                words.emplace_back(Word{letters, word.size()});
+                words.emplace_back(letters);
                 if (__builtin_popcountl(letters) == 7)
                     sevens.insert(letters);
             }
@@ -46,19 +42,19 @@ int main(int ac, char** av)
                 op(rest & -rest, pos);
         };
         int points[7] = { 0, };
-        for (Word& word : words)
-            if ((word.letters & ~seven) == 0)
-                for_letters([&seven, &word, &points](Letters letter, int pos) {
-                        if (word.letters & letter)
-                            points[pos] += (word.letters == seven) ? 3 : 1;
+        for (Letters word : words)
+            if ((word & ~seven) == 0)
+                for_letters([&](Letters letter, int pos) {
+                        if (word & letter)
+                            points[pos] += (word == seven) ? 3 : 1;
                     });
         char buf[7];
         bool any = false, mid;
-        for_letters([&points, &buf, &any, &mid](Letters letter, int pos) { 
-                any |= mid = (points[pos] > 25 && points[pos] < 33); 
+        for_letters([&](Letters letter, int pos) {
+                any |= mid = (points[pos] > 20 && points[pos] < 33);
                 buf[pos] = (mid? 'A' : 'a') + __builtin_popcountl(letter - 1);
             });
         if (any)
             std::cout << buf << '\n';
     }
-} 
+}
