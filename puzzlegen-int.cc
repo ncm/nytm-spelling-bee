ff27cc25b6afa227a15b2113fce933576eadaddc
Nathan Myers <nmyers12@bloomberg.net>
2015-08-07 18:33:44 -0400

    use unsigned instead of bitset here to get slower results

--- puzzlegen-int.cc
+++ puzzlegen-int.cc
@@ -1,6 +1,5 @@
 #include <fstream>
 #include <iostream>
-#include <bitset>
 #include <iterator>
 #include <string>
 #include <vector>
@@ -16,19 +15,20 @@ int main(int ac, char** av)
     if (!in)
         return std::cerr << "file open failed, " << name << '\n', 1;
 
-    using Letters = std::bitset<'z'-'a'+1>;
+    using Letters = unsigned;
     std::vector<Letters> words;
-    std::set<unsigned long, std::greater<>> sevens;
+    std::set<Letters, std::greater<>> sevens;
 
     for (std::istream_iterator<std::string> it(in), end; it != end; ++it) {
         if (it->size() >= 5) {
-            Letters word;
+            Letters word = 0;
             for (char c : *it)
-                (c >= 'a' && c <= 'z') ? word.set('z' - c) : word.set();
-            if (word.count() <= 7)
+                word |= (c >= 'a' && c <= 'z') ? (1u << ('z' - c)) : -1u;
+            int count = __builtin_popcountl(word);
+            if (count <= 7)
                 words.push_back(word);
-            if (word.count() == 7)
-                sevens.insert(word.to_ulong());
+            if (count == 7)
+                sevens.insert(word);
         }
     }
 
@@ -36,23 +36,23 @@ int main(int ac, char** av)
     for (Letters seven : sevens) {
         auto for_each_in_seven = [seven](auto bin_op) {
             auto least_bit = [](Letters word) -> Letters {
-                return word & Letters{-word.to_ulong()}; };
+                return word & -word; };
             int place = 0;
             Letters rest = seven;
             do { bin_op(least_bit(rest), 6 - place);
-            } while (++place, (rest &= ~least_bit(rest)).any());
+            } while (++place, (rest &= ~least_bit(rest)));
         };
         int score[7] = { 0, };
         for (Letters word : words)
-            if ((word & ~seven).none())
+            if (!(word & ~seven))
                 for_each_in_seven([&](Letters letter, int place) {
-                    if ((word & letter).any())
+                    if (word & letter)
                         score[place] += (word == seven) ? 3 : 1;
                 });
         bool any = false;
         for_each_in_seven([&](Letters letter, int place) {
             auto bit_position = [](Letters letter) {
-                return Letters{letter.to_ulong() - 1}.count(); };
+                return __builtin_popcountl(letter - 1); };
             bool middle = (score[place] > 25 && score[place] < 33);
             buf[place] = (middle ? 'Z' : 'z') - bit_position(letter);
             any |= middle;
