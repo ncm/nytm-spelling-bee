5e417eedd00f58529c7a732756013f22caa1e637
Nathan Myers <nmyers12@bloomberg.net>
2015-08-20 19:04:38 -0400

    checkpoint

--- puzzlegenrs/src/main.rs
+++ puzzlegenrs/src/main.rs
@@ -2,27 +2,31 @@ use std::io;
 use std::io::prelude::*;
 use std::env::args;
 use std::fs::File;
+use std::io::BufReader;
 use std::collections::BTreeSet;
-use std::io::BufRead;
+use std::fmt;
+// use std::io::BufRead;
 // use std::collections::Vec;
 
-use std::intrinsics::ctpop32;
-use std::intrinsics::cttz32;
-
 fn main() {
-    // let name = match args().nth(1) {
-    //     Some(n) => n,
-    //     _ => String::from("/usr/share/dict/words")
-    // };
-    // let mut fs = if name == "-" { std::io::stdin } else { File::open(name) };
-    // fs.ok().expect("file open failed, {}", name);
-    let fs = std::io::stdin;
+    let name = match args().nth(1) {
+        Some(n) => n,
+        _ => String::from("/usr/share/dict/words")
+    };
+    let mut f = if name == "-" {
+            std::io::stdin().lock().lines()
+        } else {
+            BufReader::new(File::open(name)
+                .ok().expect("file open failed")).lines()
+//              .ok().expect(format!("file open failed, {}", name))).lines()
+        };
 
     type Letters = u32;
     let mut words : Vec<Letters> = Vec::new();
     let mut sevens : BTreeSet<Letters> = BTreeSet::new();
 
-    for line in fs {
+    for l in f {
+        let line = l.unwrap();
         if line.len() >= 5 {
             let word = 0 as Letters;
             for c in line {
@@ -32,7 +36,7 @@ fn main() {
                         _ => !(0 as Letters)
                 }
             }
-            match unsafe { ctpop32(word) } {
+            match word.count_ones() {
                 7       => { sevens.insert(word); words.push(word)},
                 0 ... 6 => words.push(word),
                 _    => ()
@@ -40,37 +44,40 @@ fn main() {
         }
     }
 
-    for seven in &sevens.reverse() {
-        let mut scores  = [0; 7];
+    for seven in &sevens.iter().rev() {
+        let mut scores = [0; 7];
         for word in &words {
             if word & !seven == 0 {
                 if word == seven {
-                    scores.map_in_place(|e| e + 3);
+                    for mut points in &scores {
+                        points += 3;
+                    }
                 } else {
                     let mut rest = seven;
                     for mut points in &scores {
-                        if word & rest & -rest {
-                            points = points + 1;
+                        if word & rest & !(rest - 1) {
+                            points += 1;
                         }
-                        rest = rest & !-rest ;
+                        rest &= rest - 1;
                     }
                 }
             }
         }
         let mut any = false;
         let mut rest = seven;
-        let mut buf : [char; 7];
+        let mut buf : String::new();
+        buf = "       ";
         let mut i = 0;
-        for points in &scores {
+        for mut points in &scores {
             let z = match points { 25 ... 33 => { any = true; 'Z'}, _ => 'z' };
-            let c = (z as u8) - (unsafe { cttz32(rest & -rest) } as u8);
+            let c = (z as u8) - (rest.trailing_zeros() as u8);
             buf[6 - i] = c as char;
-            rest = rest & !-rest; 
-            i = i + 1;
+            rest &= rest - 1; 
+            i += 1;
         }
 
         if any {
-            println!("{}", buf);
+            println!("{}", String::new(buf));
         }
     }
 }
