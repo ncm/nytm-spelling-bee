803bee0756dbab911fd053976617d3eeab79890c
Nathan Myers <nmyers12@bloomberg.net>
2015-09-02 09:39:30 -0400

    pre-lock stdin, stdout

--- puzzlegen.rs
+++ puzzlegen.rs
@@ -5,8 +5,9 @@ use std::collections::BTreeSet;
 
 fn main() {
     let name = args().nth(1).unwrap_or(String::from("/usr/share/dict/words"));
+    let stdin = std::io::stdin();
     let file : Box<std::io::Read> = match &name as &str {
-        "-" => Box::new(std::io::stdin()),
+        "-" => Box::new(stdin.lock()),
          _  => Box::new(fs::File::open(name).ok().expect("file open failed"))
     };
 
@@ -25,10 +26,11 @@ fn main() {
         .filter(|word| word.count_ones() == 7)
         .collect::<BTreeSet<Letters>>();
 
-    let mut sink = std::io::BufWriter::new(std::io::stdout());
+    let stdout = std::io::stdout();
+    let mut sink = stdout.lock();
     let mut out = [0u8;8]; out[7] = '\n' as u8;
     sevens.iter().rev().all(|seven| {
-        let scores : [i32;7] = words.iter()
+        let scores = words.iter()
             .filter(|word| **word & !seven == 0)
             .map(|word| (word, if *word == *seven { 3 } else { 1 }))
             .fold([0;7],
@@ -41,16 +43,17 @@ fn main() {
                         });
                     scores
                 });
-        let (any, _, _) = scores.iter()
-            .fold((false, *seven, 6), |(any, rest, i), score| {
+        let (any_centers, _, _) = scores.iter()
+            .fold((false, *seven, 6), |(any_centers, rest, i), score| {
                 let (this, z) = match *score {
                     26 ... 32 => { (true,  'Z') },
                             _ => { (false, 'z') } };
                 out[i] = (z as u8) - (rest.trailing_zeros() as u8);
-                (any|this, rest & rest - 1, i - 1)
+                (any_centers|this, rest & rest - 1, i - 1)
             });
         if any {
             sink.write(&out).unwrap(); }
+
         true
     });
 } 
