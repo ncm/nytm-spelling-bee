b7d59375234753720133a61e2d8c83a6c1d29424
Nathan Myers <ncm@asperasoft.com>
2015-09-03 01:33:38 -0400

    make Rust code more idiomatic

--- puzzlegen.rs
+++ puzzlegen.rs
@@ -1,28 +1,28 @@
 use std::io::prelude::*;
 use std::io::{BufReader, BufWriter};
 use std::fs;
-use std::env::args;
+use std::env;
 use std::collections::BTreeSet;
 
+const WORDS_FILE : &'static str = "/usr/share/dict/words";
 type Letters = u32;
-const ZERO : Letters = 0;
-const ONE : Letters = 1;
+const NONE : Letters = 0; const Z : Letters = 1;
 
 fn main() {
-    let name = args().nth(1).unwrap_or(String::from("/usr/share/dict/words"));
+    let name = env::args().nth(1).unwrap_or(String::from(WORDS_FILE));
     let stdin = std::io::stdin();
     let file : Box<std::io::Read> = match &name as &str {
         "-" => Box::new(stdin.lock()),
-         _  => Box::new(fs::File::open(name).ok().expect("file open failed"))
+        _   => Box::new(fs::File::open(name).ok().expect("file open failed"))
     };
 
     let mut words : Vec<Letters> = Vec::new();
     let sevens = BufReader::new(file).lines()
         .filter_map(|line| match line { Ok(s) => Some(s), _ => None })
         .filter(|line| line.len() >= 5)
-        .map(|line| line.bytes().fold(ZERO, |word, c| match c as char {
-                'a' ... 'z' => word | ONE << ('z' as u8) - c,
-                _  => !ZERO
+        .map(|line| line.bytes().fold(NONE, |word, c| match c as char {
+                'a' ... 'z' => word | Z << ('z' as u8) - c,
+                _  => !NONE
          }))
         .filter(|&word| word.count_ones() <= 7)
         .inspect(|&word| words.push(word))
@@ -31,32 +31,33 @@ fn main() {
 
     let stdout = std::io::stdout();
     let mut sink = BufWriter::new(stdout.lock());
-    let mut out = [0u8;8]; out[7] = '\n' as u8;
-    sevens.iter().rev().all(|&seven| {
+    sevens.iter().rev().map(|&seven| {
         let scores = words.iter()
             .filter(|&&word| word & !seven == 0)
             .map(|&word| (word, if word == seven { 3 } else { 1 }))
             .fold([0;7], |mut scores, (word, points)| {
                 scores.iter_mut().fold(seven, |rest, score| {
                     if word & rest & !(rest - 1) != 0 {
-                        *score += points }
+                        *score += points
+                    }
                     rest & rest - 1
                 });
-
                 scores
             });
-        let found = scores.iter().fold((false, seven, 6),
-            |(found, rest, i), &score| {
-                let (z, center) = match score {
+        (seven, scores)
+    })
+    .filter_map(|(seven, scores)| {
+        let mut out = [0u8;8]; out[7] = '\n' as u8;
+        let (is_viable, _, _) = scores.iter()
+            .fold((false, seven, 6), |(is_viable, rest, i), &score| {
+                let (z, may_be_center) = match score {
                     26 ... 32 => ('Z' as u8, true),
-                    _ => ('z' as u8, false)
+                    _         => ('z' as u8, false)
                 };
                 out[i] = z - (rest.trailing_zeros() as u8);
-                (found|center, rest & rest - 1, i - 1)
+                (is_viable | may_be_center, rest & rest - 1, i - 1)
             });
-        match found {
-            (true, _, _) => { sink.write(&out).unwrap(); true },
-            _ => true
-        }
-    });
+        if is_viable { Some(out) } else { None }
+    })
+    .all(|out| { sink.write(&out).unwrap(); true });
 } 
