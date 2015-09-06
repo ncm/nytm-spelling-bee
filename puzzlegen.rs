bb83d1241da2a2a4b9f9b3868b986a4f4713081b
Nathan Myers <ncm@asperasoft.com>
2015-09-06 19:03:22 -0400

    ws

--- puzzlegen.rs
+++ puzzlegen.rs
@@ -32,8 +32,9 @@ fn main() {
     let stdout = io::stdout();
     let mut sink = io::BufWriter::new(stdout.lock());
     for &seven in sevens.iter().rev() {
-        let mut scores = [0;7];
-        for &word in words.iter() {
+        let mut scores = [0i32;7];
+        // for &word in words.iter() {
+        for &word in &words {
             if word & !seven == 0 {
                 let points = if word == seven { 3 } else { 1 };
                 let mut rest = seven;
@@ -48,12 +49,12 @@ fn main() {
         let (mut rest, mut i, mut is_viable, mut out) = (
                  seven, 0, false, [0,0,0,0,0,0,0,'\n' as u8]);
         while rest != 0 {
-                let (z, may_be_center) = match scores[i] {
-                    26 ... 32 => ('Z' as u8, true),
-                    _         => ('z' as u8, false)
-                };
-                out[6 - i] = z - (rest.trailing_zeros() as u8);
-                rest &= rest - 1; i += 1; is_viable |= may_be_center;
+            let z = match scores[i] {
+                26 ... 32 => { is_viable = true; 'Z' as u8 },
+                _         => 'z' as u8
+            };
+            out[6 - i] = z - (rest.trailing_zeros() as u8);
+            rest &= rest - 1; i += 1;
         }
         if is_viable {
              sink.write(&out).unwrap();
