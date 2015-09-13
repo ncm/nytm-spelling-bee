526937b12f525b1e42666e918a45e0ed7ee09591
Nathan Myers <ncm@asperasoft.com>
2015-09-13 12:01:38 -0400

    make -min more min

--- puzzlegen-min.cc
+++ puzzlegen-min.cc
@@ -12,19 +12,20 @@ int main(int ac, char** av)
     std::ios_base::sync_with_stdio(false);
     std::string const name = (ac == 1) ? "/usr/share/dict/words" : av[1];
     std::ifstream fs;
-    std::istream& in = (name != "-") ? (fs.open(name), fs) : std::cin;
-    if (!in)
+    std::istream& infile = (name != "-") ? (fs.open(name), fs) : std::cin;
+    if (!infile)
         return std::cerr << "file open failed, " << name << '\n', 1;
 
     using Letters = unsigned;
+    const Letters Z = 1;
     std::vector<Letters> words;
     std::set<Letters, std::greater<>> sevens;
-    for (std::istream_iterator<std::string> it(in), end; it != end; ++it) {
-        if (it->size() >= 5) {
-            Letters word = 0;
-            for (char c : *it)
-                word |= (c >= 'a' && c <= 'z') ? (1u << ('z' - c)) : -1u;
-            int count = __builtin_popcountl(word);
+    for (std::istream_iterator<std::string> in(infile), end; in != end; ++in) {
+        if (in->size() >= 5) {
+            Letters word = 0; int count = 0;
+            auto wi = in->begin(), we = in->end();
+            do  word |= (*wi >= 'a' && *wi <= 'z') ? Z << ('z' - *wi) : -Z;
+            while ((count = __builtin_popcountl(word)) <= 7 && ++wi != we);
             if (count <= 7)
                 words.push_back(word);
             if (count == 7)
@@ -32,7 +33,6 @@ int main(int ac, char** av)
         }
     }
 
-    char buf[8]; buf[7] = '\n';
     for (Letters seven : sevens) {
         int score[7] = { 0, };
         for (Letters word : words)
@@ -45,9 +45,10 @@ int main(int ac, char** av)
             }
         bool any = false;
         Letters rest = seven;
+        char buf[8] = { 0, 0, 0, 0, 0, 0, 0, '\n' };
         for (int place = 7; --place >= 0; rest &= rest - 1) {
             char z = 'z';
-            if (score[place] > 25 && score[place] < 33)
+            if (score[place] >= 26 && score[place] <= 32)
                 any = true, z = 'Z';
             buf[place] = z - __builtin_ctzl(rest);
         }
