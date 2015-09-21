1608b6e1cbeb97b153d91d610bc415e42b18f65c
Nathan Myers <ncm@asperasoft.com>
2015-09-21 00:00:19 -0400

    tiny rust refinements

--- puzzlegen.rs
+++ puzzlegen.rs
@@ -20,7 +20,7 @@ fn main() {
     let sevens : BTreeSet<_> = io::BufReader::new(file).lines()
         .filter_map(|line| line.ok())
         .filter(|line| line.len() >= 5)
-        .filter_map(|line| line.bytes().scan(NONE, |word, c| 
+        .filter_map(|line| line.bytes().scan(NONE, |word, c|
             some_if(word.count_ones() <= 7, match c as char {
                 'a' ... 'z' => { *word |= A >> c - ('a' as u8); *word }
                 _  => { *word = !NONE; *word }
@@ -32,9 +32,9 @@ fn main() {
     let stdout = io::stdout();
     let mut sink = io::BufWriter::new(stdout.lock());
     for &seven in sevens.iter().rev() {
-        let (scores, bias) = words.iter()
-            .filter(|&&word| word & !seven == 0)
-            .fold(([0u16;7], 0u16), |(mut scores, mut bias), &word| {
+        let (scores, bias) = words.iter().map(|&word| word)
+            .filter(|&word| word & !seven == 0)
+            .fold(([0u16;7], 0u16), |(mut scores, mut bias), word| {
                 if word == seven {
                     bias += 3;
                 } else {
@@ -49,10 +49,8 @@ fn main() {
         let mut out = [0, 0, 0, 0, 0, 0, 0, '\n' as u8];
         let (is_viable, _) = scores.iter().zip(out.iter_mut().rev().skip(1))
             .fold((false, seven), |(mut is_viable, rest), (&score, out)| {
-                let a = match score + bias {
-                    26 ... 32 => { is_viable = true; 'A' as u8 },
-                    _         => 'a' as u8
-                };
+                let a = match score + bias
+                    { 26 ... 32 => { is_viable = true; 'A' }, _ => 'a' } as u8;
                 *out = a + (25 - (rest.trailing_zeros() as u8));
                 (is_viable, rest & rest - 1)
             });
