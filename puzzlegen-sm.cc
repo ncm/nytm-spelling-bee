b3ef5f1f4f95c05ca7358b6b8e4cf108d2f834a0
Nathan Myers <nmyers12@bloomberg.net>
2015-09-21 17:02:42 -0400

    add C++ statemachine -sm version

---
+++ puzzlegen-sm.cc
@@ -0,0 +1,64 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <set>
+#include <functional>
+#include <streambuf>
+
+int main(int ac, char** av)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::string const name = (ac == 1) ? "/usr/share/dict/words" : av[1];
+    std::ifstream fs;
+    std::istream& file = (name != "-") ? (fs.open(name), fs) : std::cin;
+    if (!file)
+        return std::cerr << "file open failed, " << name << '\n', 1;
+
+    using Letters = unsigned;
+    const Letters A = 1 << ('z' - 'a');
+    std::vector<Letters> words;
+    std::set<Letters,std::greater<>> sevens;
+    Letters word = 0; int len = 0;
+    for (std::istreambuf_iterator<char> in(file), e; in != e; ++in) {
+        if (*in == '\n' && len < 5) {
+            word = 0; len = 0;
+        } else if (len < 0) {
+        } else if (*in == '\n') {
+            words.push_back(word);
+            if (__builtin_popcountl(word) == 7)
+                sevens.insert(word);
+            word = 0; len = 0;
+        } else if (*in >= 'a' && *in <= 'z') {
+            word |= A >> (*in - 'a');
+            len = (__builtin_popcountl(word) <= 7) ? len + 1 : -1;
+        } else { len = -1; }
+    }
+
+    for (Letters seven : sevens) {
+        short bias = 0, score[7] = { 0, };
+        for (Letters word : words)
+            if (!(word & ~seven)) {
+                if (word == seven)
+                    bias += 3;
+                else {
+                    Letters rest = seven;
+                    for (int place = 7; --place >= 0; rest &= rest - 1)
+                        if (word & rest & -rest)
+                            ++score[place];
+                }
+            }
+        bool any = false;
+        Letters rest = seven;
+        char buf[8]; buf[7] = '\n';
+        for (int place = 7; --place >= 0; rest &= rest - 1) {
+            const int points = score[place] + bias;
+            const char a = (points >= 26 && points <= 32) ?
+                any = true, 'A' : 'a';
+            buf[place] = a + (25 - __builtin_ctzl(rest));
+        }
+        if (any)
+            std::cout.rdbuf()->sputn(buf, 8);
+    }
+    return 0;
+}
