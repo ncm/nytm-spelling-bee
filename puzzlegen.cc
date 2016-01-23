f56802a32588706703bab6ff1a6cab0479ee5fdc
Nathan Myers <ncm@asperasoft.com>
2016-01-23 16:31:53 -0500

    replace map/BTreeSet with sorting, modernize, improve Rust-side arg handling

--- puzzlegen.cc
+++ puzzlegen.cc
@@ -3,9 +3,8 @@
 #include <iterator>
 #include <bitset>
 #include <vector>
-#include <map>
-#include <functional>
 #include <string>
+#include <algorithm>
 
 extern "C" int main(int ac, char** av)
 {
@@ -13,17 +12,16 @@ extern "C" int main(int ac, char** av)
     std::ifstream fs;
     std::istream& file = name == "-" ? std::cin : (fs.open(name), fs);
     if (!file)
-        return std::cerr << "file open failed, " << name << '\n', 1;
+        return std::cerr << "file open failed, \"" << name << "\"\n", 1;
 
-    using Letters = std::bitset<32>;
     std::vector<unsigned> words;
-    std::map<unsigned,int,std::greater<>> sevens;
-    Letters word; int len = 0;
+    std::vector<std::pair<unsigned,short>> sevens;
+    std::bitset<32> word; int len = 0;
     for (std::istreambuf_iterator<char> in(file), e; in != e; ++in)
         if (*in == '\n') {
             if (len >= 5) {
                 if (word.count() == 7) {
-                    ++sevens[word.to_ulong()];
+                    sevens.emplace_back(word.to_ulong(), 0);
                 } else words.push_back(word.to_ulong());
             }
             word = 0, len = 0;
@@ -31,6 +29,13 @@ extern "C" int main(int ac, char** av)
             word.set(25 - (*in - 'a'));
             len = (word.count() <= 7) ? len + 1 : -1;
         } else { len = -1; }
+    std::sort(sevens.begin(), sevens.end(), [](auto& a, auto& b)
+        { return a.first > b.first; });
+    auto p = sevens.begin();
+    for (auto s = p; s != sevens.end(); ++p->second, ++s)
+        if (s->first != p->first)
+            *++p = *s;
+    sevens.resize(p + 1 - sevens.begin());
 
     for (auto sevencount : sevens) {
         unsigned const seven = sevencount.first;
@@ -49,7 +54,7 @@ extern "C" int main(int ac, char** av)
         for (int place = 7; --place >= 0; rest &= rest - 1) {
             int points = scores[place] + sevencount.second * 3;
             char a = (points >= 26 && points <= 32) ? any = true, 'A' : 'a';
-            buf[place] = a + (25 - Letters(~rest & (rest - 1)).count());
+            buf[place] = a + (25 - std::bitset<32>(~rest & (rest - 1)).count());
         }
         if (any)
             std::cout.rdbuf()->sputn(buf, 8);
--- puzzlegen.rs
+++ puzzlegen.rs
@@ -1,46 +1,48 @@
 use std::io::prelude::*;
-use std::{fs,io,env};
-use std::collections::BTreeMap;
+use std::{fs,io,env,process};
 
 #[no_mangle] pub extern fn rs_main() {
-    let name = env::args().nth(1).unwrap_or(
+    let filename = env::args().nth(1).unwrap_or(
         String::from("/usr/share/dict/words"));
     let stdin = io::stdin();
-    let file : Box<io::Read> = match &*name {
+    let file : Box<io::Read> = match &*filename {
         "-" => Box::new(stdin.lock()),
-        _   => Box::new(fs::File::open(name).ok().expect("file open failed"))
+        _ => match fs::File::open(&*filename) {
+            Ok(f) => Box::new(f), Err(x) => {
+               writeln!(io::stderr(), "{}: \"{}\"", x, filename).unwrap();
+               process::exit(1)
+            }
+        }
     };
 
-    // let mut sevens : BTreeMap<u32,u16> = BTreeMap::new();
-    let mut sevens = BTreeMap::new();
-    let words : Vec<_> = io::BufReader::new(file).bytes()
-        .filter_map(Result::ok)
-        .scan((0u32, 0), |&mut (ref mut word, ref mut len), c| {
-            match c as char {
-                '\n' => {
-                   let (w, l) = (*word, *len); *word = 0; *len = 0;
-                   if l >= 5
-                       { return Some(Some(w)) }
-                },
-                'a' ... 'z' if *len != -1 => {
-                   let w = *word | 1 << (25 - (c - b'a'));
-                   if w.count_ones() <= 7 {
-                       *word = w; *len += 1
-                   } else { *len = -1 }
-                },
-                _ =>  { *len = -1 }
-            }; Some(None)
-        }).filter_map(|option| option).filter(|&word|
-            if word.count_ones() == 7 {
-                *sevens.entry(word).or_insert(0) += 1; false
-            } else { true }
-        ).collect();
+    let (mut sevens, mut words) = (Vec::new(), Vec::new());
+    let (mut word, mut len) = (0u32, 0);
+    for c in io::BufReader::new(file).bytes().filter_map(Result::ok) {
+        if c == b'\n' {
+            if len >= 5 {
+                if word.count_ones() == 7 {
+                        sevens.push(word)
+                } else { words.push(word) }
+            }
+            word = 0; len = 0;
+        } else if len != -1 && c >= b'a' && c <= b'z' {
+            word |= 1 << (25 - (c - b'a'));
+            len = if word.count_ones() <= 7 { len + 1 } else { -1 }
+        } else { len = -1 }
+    }
+    sevens.sort();
+    let sevens : Vec<_> = sevens.iter().skip(1).chain([0].iter())
+        .scan((sevens[0], 1u16), |&mut (ref mut prev, ref mut count), &seven|
+            if seven != *prev {
+               let pair = (*prev, *count); *prev = seven; *count = 1;
+               Some(Some(pair))
+            } else { *count += 1; Some(None) }
+         ).filter_map(|pair| pair).collect();
 
     let stdout = io::stdout();
     let mut sink = io::BufWriter::new(stdout.lock());
-    for (&seven,&count) in sevens.iter().rev() {
-        let scores = words.iter()
-            .filter(|&word| word & !seven == 0)
+    for &(seven, count) in sevens.iter().rev() {
+        let scores = words.iter().filter(|&word| word & !seven == 0)
             .fold([0u16;7], |mut scores, word| {
                 scores.iter_mut().fold(seven, |rest, score| {
                    if word & rest & !(rest - 1) != 0
@@ -49,13 +51,12 @@ use std::collections::BTreeMap;
                 });
                 scores
             });
-
         let mut out = *b".......\n";
         let (any, _) = scores.iter().zip(out.iter_mut().rev().skip(1))
-            .fold((false, seven), |(mut any, rest), (&score, out)| {
+            .fold((false, seven), |(mut any, rest), (&score, outc)| {
                 let a = match score + 3 * count
                     { 26 ... 32 => { any = true; 'A' }, _ => 'a' };
-                *out = (a as u8) + (25 - rest.trailing_zeros()) as u8;
+                *outc = a as u8 + (25 - rest.trailing_zeros()) as u8;
                 (any, rest & rest - 1)
             });
         if any
