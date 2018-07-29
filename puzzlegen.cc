6bd2eb9e358fe7c5b3b3a283037864753cea7d78
Nathan Myers <ncm@cantrip.org>
2018-07-29 15:39:03 -0400

    Update to more modern and tighter style
    Use modern loops
    Remove dependence on std::string
    Use output buffer during scoring
    Use bitset<>::count less.

--- puzzlegen.cc
+++ puzzlegen.cc
@@ -1,61 +1,60 @@
 #include <iostream>
 #include <fstream>
-#include <iterator>
 #include <bitset>
 #include <vector>
-#include <string>
 #include <algorithm>
 
 extern "C" int main(int argc, char** argv)
 {
-    std::string name = (argc > 1) ? argv[1] : "/usr/share/dict/words";
+    char const* fn = (argc > 1) ? argv[1] : "/usr/share/dict/words";
     std::ifstream fs;
-    std::istream& file = (name == "-") ? std::cin : (fs.open(name), fs);
+    std::istream& file = (*fn != '-' || fn[1]) ? fs.open(fn), fs : std::cin;
     if (!file)
-        return std::cerr << "file open failed, \"" << name << "\"\n", 1;
+        return std::cerr << "file open failed, \"" << fn << "\"\n", 1;
 
-    std::vector<unsigned> sevens; sevens.reserve(1<<14);
-    std::vector<unsigned> words; words.reserve(1<<15);
-    std::bitset<32> word; int len = 0; int ones = 0;
+    struct pangram { int word; short score; };
+    std::vector<pangram> pangrams; pangrams.reserve(1<<14);
+    std::vector<int> words; words.reserve(1<<15);
+    int word = 0, len = 0, ones = 0;
     for (std::istreambuf_iterator<char> in(file), eof; in != eof; ++in) {
-        if (*in == '\n') {
-            if (len >= 5 && ones <= 7)
-                (ones == 7 ? sevens : words).push_back(word.to_ulong());
+        if (*in == '\n' || *n == '\r') {
+            if (len >= 5 && ones <= 7) {
+                if (ones == 7) pangrams.push_back({word, 0});
+                else words.push_back(word);
+            }
             word = len = ones = 0;
         } else if (ones != 8 && *in >= 'a' && *in <= 'z') {
-            ++len, ones = word.set(25 - (*in - 'a')).count();
+            int bit = (1<<25) >> (*in - 'a');
+            ++len, ones += ((~word & bit) != 0), word |= bit;
         } else { ones = 8; }
     }
 
-    std::sort(sevens.begin(), sevens.end());
-    std::vector<short> counts(sevens.size());
-    int count = -1; unsigned prev = 0;
-    for (auto seven : sevens) {
-        if (prev != seven)
-            sevens[++count] = prev = seven;
-        counts[count] += 3;
+    std::sort(pangrams.begin(), pangrams.end(),
+        [](auto a, auto b) { return a.word > b.word; });
+    int back = 0, prev = pangrams.front().word;
+    for (auto gram : pangrams) {
+        if (prev != gram.word)
+            pangrams[++back].word = prev = gram.word;
+        pangrams[back].score += 3;
     }
+    pangrams.resize(back + 1);
 
-    for (; count >= 0; --count) {
-        unsigned const seven = sevens[count];
-        short bits[7], scores[7];
-        for (unsigned rest = seven, place = 7; place-- != 0; rest &= rest - 1) {
-            bits[place] = std::bitset<32>((rest & ~(rest - 1)) - 1).count();
-            scores[place] = counts[count];
-        }
-        for (unsigned word : words)
-            if (!(word & ~seven))
-                for (int place = 0; place < 7; ++place)
-                    scores[place] += (word >> bits[place]) & 1;
+    for (auto gram : pangrams) {
+        char out[8] = { 0, };
+        for (int pos = 7, rest = gram.word; pos-- != 0; rest &= rest - 1)
+            out[pos] = 25 - std::bitset<32>((rest & ~(rest - 1)) - 1).count();
+        short scores[7] = { 0, };
+        for (int word : words)
+            if (!(word & ~gram.word))
+                for (int pos = 0; pos < 7; ++pos)
+                    scores[pos] += (word & ((1<<25) >> out[pos])) != 0;
 
-        bool any = false; char out[8];
-        for (int place = 0; place != 7; ++place) {
-            int points = scores[place];
-            char a = (points >= 26 && points <= 32) ? any = true, 'A' : 'a';
-            out[place] = a + (25 - bits[place]);
+        for (int pos = 0; pos < 7; ++pos) {
+            int sc = scores[pos] + gram.score;
+            out[pos] += (sc >= 26 && sc <= 32) ? out[7] = '\n', 'A' : 'a';
         }
-        if (any)
-             out[7] = '\n', std::cout.rdbuf()->sputn(out, 8);
+        if (out[7])
+            std::cout.rdbuf()->sputn(out, 8);
     }
     return 0;
 }
